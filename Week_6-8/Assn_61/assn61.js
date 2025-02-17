4faac9886ac34722a7f9f6a76125421216191d9e
SNDev11 <saisourabhnanda@gmail.com>
2025-02-17 20:15:32 +0530

    Assn61 completed

---
+++ Week_6-8/Assn_61/assn61.js
@@ -0,0 +1,7 @@
+function concat_Strings(str1, str2){
+    let new_str1= str1.slice(1);
+    let new_str2= str2.slice(1);
+    return new_str1 + new_str2;
+
+}
+console.log(concat_Strings("Swasti", "Karishma"));
